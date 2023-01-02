ca8ee5b8f4f40b7c2cf9d877be302da86424f2e3
BloodySteps <codewithtyson@gmail.com>
2023-01-02 22:16:45 +0400

    feat: progress-bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,19 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {Inter} from '@next/font/google';
 
+import NextNProgress from 'nextjs-progressbar';
+
 const inter = Inter({
   variable: '--font-inter',
 });
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <div className={inter.variable}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <NextNProgress color="#7c09ff" />
+      <div className={inter.variable}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
