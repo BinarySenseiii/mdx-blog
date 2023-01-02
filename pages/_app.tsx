8f1d01b9c23f55a4630ad9cdf6b983fd6d995ed7
BloodySteps <codewithtyson@gmail.com>
2023-01-02 10:45:50 +0400

    feat: get-slugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import {Inter} from '@next/font/google';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({
+  variable: '--font-inter',
+});
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <div className={inter.variable}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
