65f73a62aeb518aeb484621f11ef5df1bde3ab0a
BloodySteps <codewithtyson@gmail.com>
2023-01-14 22:22:27 +0400

    fixed: seo

--- lib/seo.ts
+++ lib/seo.ts
@@ -3,8 +3,7 @@ import type { DefaultSeoProps } from 'next-seo';
 
 const title = `Faisal`;
 const description = `Welcome to my digital garden where I share what I'm learning about shipping great products, becoming a better developer and growing a career in tech.`;
-const domain = `delba.dev`;
-const twitter = `@delba_oliveira`;
+const domain = `mdx-blog`;
 const meta = `Developer Experience at ColossalBit.io`;
 
 export const seo: DefaultSeoProps = {
@@ -23,9 +22,5 @@ export const seo: DefaultSeoProps = {
         alt: title,
       },
     ],
-  },
-  twitter: {
-    handle: twitter,
-    cardType: 'summary_large_image',
   },
 };
