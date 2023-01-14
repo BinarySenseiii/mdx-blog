8c44f00db9d92aee9ed20349d8fb404a90272c31
BloodySteps <codewithtyson@gmail.com>
2023-01-14 22:32:19 +0400

    remove - unused seo var

--- lib/seo.ts
+++ lib/seo.ts
@@ -2,7 +2,6 @@ import type { DefaultSeoProps } from 'next-seo';
 
 const title = `Faisal`;
 const description = `Welcome to my digital garden where I share what I'm learning about shipping great products, becoming a better developer and growing a career in tech.`;
-const domain = `mdx-blog`;
 const meta = `Developer Experience at ColossalBit.io`;
 
 export const seo: DefaultSeoProps = {
